1f9f46c390a44e2cfd821bce5b050b3f1690bce7
Kamil Niedziela <kaminie318@student.polsl.pl>
2022-06-12 23:52:28 +0200

    Little refactor

--- src/main/java/main/Main.java
+++ src/main/java/main/Main.java
@@ -5,7 +5,7 @@ import gui.Gui;
 import java.io.IOException;
 
 public class Main {
-    public static void main(String[] args) throws IOException {
+    public static void main(String[] args) {
         java.awt.EventQueue.invokeLater(new Runnable() {
             public void run() {
                 new Gui();
